8eee7da7d05b6bf896b65e4306dd16adebe63794
Robert Stachowski <RobertStachowski0@gmail.com>
2025-08-07 10:51:25 +0200

    Adjust make_sound() mthods to match task output format

--- 04/app.py
+++ 04/app.py
@@ -9,21 +9,21 @@ class Dog(Animal):
     def __init__(self, name):
         super().__init__(name)
     def make_sound(self):
-        return "Hau hau"
+        return f"{self.name} szczeka: Hau hau"
 
 class Cat(Animal): 
     def __init__(self, name):
         super().__init__(name) 
     def make_sound(self):
-        return "Miau miau"
+        return f"{self.name} miauczy: Miau miau"
     
 class Bird(Animal): 
     def __init__(self, name):
         super().__init__(name) 
     def make_sound(self):
-        return "Ćwir ćwir"
+        return f"{self.name} ćwierka: Ćwir ćwir"
 
 animals = [Dog("Mruczek"), Dog("Reksio"), Cat("Filemon"), Cat("Bonifacy"), Bird("Ptaszek 1"), Bird("Ptaszek 2")]
 
 for a in animals:
-    print(f"{a.name} wydaje dźwięk: {a.make_sound()}")
+    print(a.make_sound())
